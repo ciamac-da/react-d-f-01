5317691d6d71b28b4e6b217e48bb714fc1ddc812
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-28 09:54:40 +0200

    Verified by Marti4n v1.0.2 useReducer added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,11 +6,17 @@ import Button from '../UI/Button/Button';
 
 
 const emailReducer = (state, action) => {
+  if(action.type === "USER_INPUT") {
+    return {value:action.val, isValid: action.val.includes("@")};
+  }
+  if(action.type === "USER_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@")}; 
+  }
 return {value:"", isValid: false}
 }
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  /* const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState(); */
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
@@ -42,7 +48,8 @@ const Login = (props) => {
   // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+   /*  setEnteredEmail(event.target.value); */
+   dispatchEmail({type:"USER_INPUT", val: event.target.value})
 
     setFormIsValid(
       event.target.value.includes('@') && enteredPassword.trim().length > 6
@@ -60,7 +67,8 @@ const Login = (props) => {
 
   const validateEmailHandler = () => {
     //setEmailIsValid(enteredEmail.includes('@'));
-    setEmailIsValid(emailState.isValid);
+    //setEmailIsValid(emailState.isValid);
+    dispatchEmail({type:"INPUT_BLUR"});
   };
 
   const validatePasswordHandler = () => {
@@ -77,14 +85,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
