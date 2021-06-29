b6e4f6b449ff551f80075dfbad23079d96081199
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-29 09:10:34 +0200

    Verified by Marti4n v1.0.3 useReducer done

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h2>A Typical Page</h2>
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
